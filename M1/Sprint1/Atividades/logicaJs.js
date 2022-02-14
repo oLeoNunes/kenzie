4e195ca88e6c32b5aaf79f5ec4515894a721d596
nunesleo04@gmail.com <nunesleo04@gmail.com>
2022-02-14 13:49:58 -0300

    organizacao

---
+++ M1/Sprint1/Atividades/logicaJs.js
@@ -0,0 +1,18 @@
+let meuNome = "Leonardo";
+comprimentoMeuNome = meuNome.length;
+
+console.log(comprimentoMeuNome)
+
+let preco = 15
+
+let precoFormatado = "R$15,00"
+
+console.log(`O valor ${preco} formatado ficaria assim: ${precoFormatado}`)
+
+
+
+let cumprimento = "Bom dia"
+let nome = "Leonardo"
+let sobreNome = "Nunes"
+
+console.log(cumprimento + ' ' + nome + ' ' + sobreNome)
---
+++ M1/Sprint2/S2A04  💪 Atividade - Javascript Funções/funcaoJs.js
@@ -0,0 +1,18 @@
+function somaNumeros(a, b){
+    let resultadoSoma = a + b;
+    return resultadoSoma;
+}
+
+
+let resultado = somaNumeros(5, 8);
+let resultado2 = somaNumeros(2, 3);
+
+console.log(resultado);
+console.log(resultado2);
+function subtraiNumeros(a, b) {
+    let resultadoSubtracao = a-b;
+    return resultadoSubtracao;
+}
+console.log(subtraiNumeros(5, 2));
+console.log(subtraiNumeros(9, 5));
+
